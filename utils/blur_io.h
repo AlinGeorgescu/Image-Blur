7c6c25759ae59585171b2127234a7c6ef4b55af8
alin.georgescu <alin.georgescu@stud.acs.upb.ro>
2020-11-20 10:45:21 +0200

    Add IO and filter headers

---
+++ utils/blur_io.h
@@ -0,0 +1,104 @@
+/**
+ * (C) Copyright 2020 - Proiect APP
+ *                      Blurarea imaginilor
+ * Georgescu Alin-Andrei 342 C3
+ * Iuga Florin-Eugen     342 C5
+ * Negru Bogdan-Crisitan 342 C3
+ */
+
+#ifndef BLUR_IO_H_
+#define BLUR_IO_H_
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include "blur_utils.h"
+
+#define MAX_LEN  255
+
+/**
+ * Funcția de citire din fișier a unei imagini.
+ *
+ * @param image_in = denumirea fișierului
+ * @param type     = adresa tipului imaginii (PNM / PGM)
+ * @param channels = adresa numărului de canale al imaginii
+ * @param width    = adresa lățimii imaginii
+ * @param height   = adresa înălțimii imaginii
+ * @param maxval   = adresa valorii maxime a unui pixel
+ * @param image    = adresa matricei imaginii
+ */
+void read_image(char *image_in,
+                uint8_t *type, uint8_t *channels,
+                int *width, int *height,
+                uint8_t *maxval, uint8_t ***image) {
+    FILE *in;
+    char comment_line[MAX_LEN];
+    char c;
+    int i;
+
+    in = fopen(image_in, "rb");
+    check_file(in);
+
+    fscanf(in, "%c %hhu", &c, type);
+    fgets(comment_line, MAX_LEN, in);
+    fgets(comment_line, MAX_LEN, in);
+    fscanf(in, "%d %d %hhu", width, height, maxval);
+    fgets(comment_line, MAX_LEN, in);
+
+    if ((*type) == 5) {
+        *channels = 1;
+    } else {
+        *channels = 3;
+    }
+
+    *image = (uint8_t **) malloc(((*height) + 2) * sizeof(uint8_t *));
+    check_container(*image, in);
+
+    for (i = 0; i < (*height) + 2; ++i) {
+        (*image)[i] =
+            (uint8_t *) calloc((*channels) * ((*width) + 2), sizeof(uint8_t));
+        check_container((*image)[i], in);
+    }
+
+    for (i = 1; i < (*height) + 1; ++i) {
+        fread((*image)[i] + (*channels), sizeof(uint8_t),
+              (*width) * (*channels), in);
+    }
+
+    fclose(in);
+}
+
+/**
+ * Funcția de scriere în fișier a unei imagini.
+ *
+ * @param image_out = denumirea fișierului
+ * @param type      = adresa tipului imaginii (PNM / PGM)
+ * @param channels  = adresa numărului de canale al imaginii
+ * @param width     = adresa lățimii imaginii
+ * @param height    = adresa înălțimii imaginii
+ * @param maxval    = adresa valorii maxime a unui pixel
+ * @param image     = matricea imaginii
+ */
+void print_image(char *image_out,
+                uint8_t *type, uint8_t *channels,
+                int *width, int *height,
+                uint8_t *maxval, uint8_t **image) {
+    FILE *out;
+    int i;
+
+    out = fopen(image_out, "wb");
+    check_file(out);
+
+    fprintf(out, "P%u\n", *type);
+    fprintf(out, "%d %d\n", *width, *height);
+    fprintf(out, "%d\n", *maxval);
+
+    for (i = 1; i < (*height) + 1; ++i) {
+        fwrite(image[i] + (*channels), sizeof(uint8_t),
+               (*width) * (*channels), out);
+    }
+
+    fclose(out);
+}
+
+#endif  // BLUR_IO_H_
---
+++ utils/blur_utils.h
@@ -0,0 +1,114 @@
+/**
+ * (C) Copyright 2020 - Proiect APP
+ *                      Blurarea imaginilor
+ * Georgescu Alin-Andrei 342 C3
+ * Iuga Florin-Eugen     342 C5
+ * Negru Bogdan-Crisitan 342 C3
+ */
+
+#ifndef BLUR_UTILS_H_
+#define BLUR_UTILS_H_
+
+#include <stdio.h>
+
+#ifdef MPI_VERSION
+#include "mpi.h"
+#endif  // MPI_VERSION
+
+#define MIN(a, b)  (((a) < (b)) ? (a) : (b))
+#define MAX(a, b)  (((a) > (b)) ? (a) : (b))
+
+// Matricea filtrului Blur Gaussian
+static const float GAUSSIAN[3][3] = {
+    {1.f / 16, 2.f / 16, 1.f / 16},
+    {2.f / 16, 4.f / 16, 2.f / 16},
+    {1.f / 16, 2.f / 16, 1.f / 16}
+};
+
+/**
+ * Verifică dacă fișierul s-a deschis cu succes sau nu.
+ *
+ * @param file = descriptorul de fișier
+ */
+void check_file(FILE *file) {
+    if (!file) {
+        fprintf(stderr, "Eroare la deschiderea fișierelor!\n");
+
+        #ifdef MPI_VERSION
+        MPI_Finalize();
+        #endif  // MPI_VERSION
+
+        exit(-1);
+    }
+}
+
+/**
+ * Verifică dacă s-a putut aloca memorie pentru structura de date sau nu.
+ *
+ * @param ptr  = pointerul către container
+ * @param file = fișierul ce trebuie înschis înainte de închiderea programului
+ */
+void check_container(void *ptr, FILE *file) {
+    if (!ptr) {
+        fprintf(stderr, "Eroare la alocarea memoriei!\n");
+        if (file) {
+            fclose(file);
+        }
+
+        #ifdef MPI_VERSION
+        MPI_Finalize();
+        #endif  // MPI_VERSION
+
+        exit(-1);
+    }
+}
+
+/**
+ * Aplică filtrul pe o imagine / bucată de imagine.
+ *
+ * @param height   = înălțimea fragmentului de imagine editat
+ * @param width    = lățimea fragmentului de imagine editat
+ * @param channels = numărul de canale al imaginii (color / alb-negru)
+ * @param maxval   = valoarea maximă a unui pixel
+ * @param image    = matricea imaginii
+ */
+void apply_filter(int height, int width, uint8_t channels, uint8_t maxval,
+                  uint8_t **image) {
+    int i, j;
+    float pixel;
+    uint8_t **aux;
+
+    aux = (uint8_t **) malloc((height + 2) * sizeof(uint8_t *));
+    check_container(aux, NULL);
+
+    for (i = 0; i < height + 2; ++i) {
+        aux[i] = (uint8_t *) malloc(channels * (width + 2) * sizeof(uint8_t));
+        check_container(aux[i], NULL);
+        memcpy(aux[i], image[i], channels * (width + 2) * sizeof(uint8_t));
+    }
+
+    // Aplicarea filtrului propriu-zis (a convoluției)
+    for (i = 1; i < height + 1; ++i) {
+        for (j = channels; j < channels * (width + 1); ++j) {
+            pixel = GAUSSIAN[2][2] * aux[i - 1][j - channels] +
+                    GAUSSIAN[2][1] * aux[i - 1][j]            +
+                    GAUSSIAN[2][0] * aux[i - 1][j + channels] +
+                    GAUSSIAN[1][2] * aux[i][j - channels]     +
+                    GAUSSIAN[1][1] * aux[i][j]                +
+                    GAUSSIAN[1][0] * aux[i][j + channels]     +
+                    GAUSSIAN[0][2] * aux[i + 1][j - channels] +
+                    GAUSSIAN[0][1] * aux[i + 1][j]            +
+                    GAUSSIAN[0][0] * aux[i + 1][j + channels];
+            pixel = MIN(maxval, pixel);
+            pixel = MAX(0, pixel);
+            image[i][j] = pixel;
+        }
+    }
+
+    for (i = 0; i < height + 2; ++i) {
+        free(aux[i]);
+    }
+    free(aux);
+}
+
+#endif  // BLUR_UTILS_H_
